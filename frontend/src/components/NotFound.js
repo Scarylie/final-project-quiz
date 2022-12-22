a09b0b2a7c9c08f4298be54e75d97c61aff623f7
YoTesfam <102465267+YoTesfam@users.noreply.github.com>
2022-12-22 11:14:48 +0100

    Update NotFound.js
    New component

--- frontend/src/components/NotFound.js
+++ frontend/src/components/NotFound.js
@@ -1,10 +1,7 @@
 import React from 'react';
 
-export const NotFound = () => {
-  return (
-    <div>
-      <h1>Error code: 404</h1>
-      <h2>Page not found</h2>
-    </div>
-  );
+const NotFound = () => {
+  return <p>Sorry, page not found.</p>;
 };
+
+export default NotFound;
