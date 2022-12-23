8aeb4d80db84ac6b7ec3007b1fee7f4215586eaa
Scarylie <saralie.bognandi@gmail.com>
2022-12-23 11:17:03 +0100

    changed name from user to auth
    Co-authored-by: Sarah Kneedler <Kneedler@users.noreply.github.com>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 
-import user from 'reducers/user';
+import user from 'reducers/auth';
 import { StartPage } from 'components/StartPage';
 import LogIn from 'components/auth/LogIn';
 import Register from 'components/auth/Register';
--- frontend/src/components/auth/Register.js
+++ frontend/src/components/auth/Register.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector, batch } from 'react-redux';
 import { useNavigate, Link } from 'react-router-dom';
 import { API_URL } from 'utils/user';
-import user from 'reducers/user';
+import user from 'reducers/auth';
 // Styles
 import {
   Container,
