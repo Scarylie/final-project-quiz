036c2a580c4be99124d691b0308e2871eb3d5f71
YoTesfam <102465267+YoTesfam@users.noreply.github.com>
2023-01-11 20:21:50 +0100

    git
    Co-authored-by: Saralie Bognandi <Scarylie@users.noreply.github.com>
    Co-authored-by: Sarah Kneedler <Kneedler@users.noreply.github.com>

--- frontend/src/components/quiz/playquiz/index.js
+++ frontend/src/components/quiz/playquiz/index.js
@@ -114,7 +114,7 @@ const PlayQuiz = () => {
   // useEffect(() => {
   //   calculateScore();
   // }, [score]);
-
+  const [buttonText, setButtonText] = useState("Finished");
   return (
     <Container>
       {state === 'intro' && (
@@ -123,9 +123,9 @@ const PlayQuiz = () => {
             <PageHeading>Start game</PageHeading>
             <PageSubHeading>{quiz.title}</PageSubHeading>
             <p>{quiz.creator}</p>
-            <button type="button" onClick={() => setState('isPlaying')}>
+            <StyledButton><button className='button' type="button" onClick={() => setState('isPlaying')}>
               Play
-            </button>
+            </button></StyledButton>
           </IntroContent>
         </IntroContainer>
       )}
@@ -163,13 +163,15 @@ const PlayQuiz = () => {
                     })}
                     <div>
                       {index === quiz.questions.length - 1 ? (
-                        <button
+                        <StyledButton><button className='button'
                           type="button"
-                          onClick={(event) =>
+                          onClick={(event) => {
                             handleFinishQuiz(event, currentQuestion)
-                          }>
-                          Finish
-                        </button>
+                            
+                              setButtonText("Submit");
+                          }}>
+                          {buttonText}
+                        </button></StyledButton>
                       ) : (
                         <button
                           type="button"
@@ -228,3 +230,107 @@ const Img = styled.img`
     width: 450px;
   }
 `;
+
+const StyledButton = styled.button `
+
+.button {
+  align-items: center;
+  appearance: none;
+  background-clip: padding-box;
+  background-color: initial;
+  background-image: none;
+  border-style: none;
+  box-sizing: border-box;
+  color: #fff;
+  cursor: pointer;
+  display: inline-block;
+  flex-direction: row;
+  flex-shrink: 0;
+  /* font-family: Eina01,sans-serif; */
+  font-size: 16px;
+  font-weight: 800;
+  justify-content: center;
+  line-height: 24px;
+  margin: 0;
+  min-height: 64px;
+  outline: none;
+  overflow: visible;
+  padding: 19px 26px;
+  pointer-events: auto;
+  position: relative;
+  text-align: center;
+  text-decoration: none;
+  text-transform: none;
+  user-select: none;
+  -webkit-user-select: none;
+  touch-action: manipulation;
+  vertical-align: middle;
+  width: auto;
+  word-break: keep-all;
+  z-index: 0;
+}
+
+@media (min-width: 768px) {
+  .button {
+    padding: 19px 32px;
+  }
+}
+
+.button:before,
+.button:after {
+  border-radius: 80px;
+}
+
+.button:before {
+  background-color: rgba(249, 58, 19, .32);
+  content: "";
+  display: block;
+  height: 100%;
+  left: 0;
+  overflow: hidden;
+  position: absolute;
+  top: 0;
+  width: 100%;
+  z-index: -2;
+}
+
+.button:after {
+  background-color: initial;
+  background-image: linear-gradient(92.83deg, #ff7426 0, #f93a13 100%);
+  bottom: 4px;
+  content: "";
+  display: block;
+  left: 4px;
+  overflow: hidden;
+  position: absolute;
+  right: 4px;
+  top: 4px;
+  transition: all 100ms ease-out;
+  z-index: -1;
+}
+
+.button:hover:not(:disabled):after {
+  bottom: 0;
+  left: 0;
+  right: 0;
+  top: 0;
+  transition-timing-function: ease-in;
+}
+
+.button:active:not(:disabled) {
+  color: #ccc;
+}
+
+.button:active:not(:disabled):after {
+  background-image: linear-gradient(0deg, rgba(0, 0, 0, .2), rgba(0, 0, 0, .2)), linear-gradient(92.83deg, #ff7426 0, #f93a13 100%);
+  bottom: 4px;
+  left: 4px;
+  right: 4px;
+  top: 4px;
+}
+
+.button:disabled {
+  cursor: default;
+  opacity: .24;
+}
+`
