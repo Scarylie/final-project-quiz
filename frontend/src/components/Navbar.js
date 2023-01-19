729429505e3d0781f0d34fd92e8ea9f6bb00a184
Scarylie <saralie.bognandi@gmail.com>
2023-01-19 22:03:01 +0100

    style

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -61,7 +61,7 @@ const Navbar = () => {
         <Logos>
           <div>
             {accessToken ? (
-              <Link to={`/home`}>
+              <Link aria-label="Link to home page" to={`/home`}>
                 <AiOutlinePlayCircle style={iconStyles} />
               </Link>
             ) : (
@@ -70,22 +70,26 @@ const Navbar = () => {
           </div>
           <div>
             {accessToken ? (
-              <Link to={`/create`}>
+              <Link aria-label="Link to create quiz" to={`/create`}>
                 <AiOutlinePlusCircle style={iconStyles} />
               </Link>
             ) : (
-              <Link to={`/about`}>
+              <Link aria-label="Link to start page" to={`/about`}>
                 <VscInfo style={iconStyles} />
               </Link>
             )}
           </div>
           <LinkWrapper>
             {accessToken ? (
-              <Link to={`/profile`}>
+              <Link aria-label="Link to profile page" to={`/profile`}>
                 <ProfileIconWrapper>
-                  <BsFillPersonFill style={iconStyles} /> {username && username}
+                  <BsFillPersonFill style={iconStyles} />{' '}
+                  <UserName>{username && username}</UserName>
                   <Menu className="profile-menu">
-                    <GhostBtn type="button" onClick={onLogOut}>
+                    <GhostBtn
+                      aria-label="Log out button"
+                      type="button"
+                      onClick={onLogOut}>
                       <LinkWrapper>
                         Logout <RiLogoutBoxRLine style={iconStyles} />
                       </LinkWrapper>
@@ -132,9 +136,15 @@ const Menu = styled.div`
   display: none;
   position: absolute;
   background: #bb5ff7;
-  bottom: -45px;
-  padding: 10px;
-  right: 0;
+  border-radius: 10px 10px 0 0;
+  bottom: 30px;
+  padding: 7px;
+  right: -20px;
+
+  @media (min-width: 600px) {
+    bottom: -40px;
+    border-radius: 0 0 10px 10px;
+  }
 `;
 
 const Logos = styled.div`
@@ -146,4 +156,11 @@ const Logos = styled.div`
 const Img = styled.img`
   width: 30px;
   margin-left: 30px;
+`;
+
+const UserName = styled.div`
+  display: flex;
+  text-transform: capitalize;
+  align-self: flex-end;
+  padding-bottom: 2px;
 `;
